bb2fa86e5f5acaa51693f9d9d5a6d39351b5800c
MIAE <kma613@naver.com>
2018-07-24 00:58:53 +0900

    '구구단'

--- study01/First-Java-Review/ch05/ByTimes2.java
+++ study01/First-Java-Review/ch05/ByTimes2.java
@@ -1,10 +1,20 @@
 //while문으로 재구성 해보기
+// for(int i=2; i<10; i++){
+//     for(int j=1; j<10; j++)
+//         System.out.println(i + "x" + j + "=" + i*j);
+            
 class ByTimes2{
     public static void main(String[] args){
-        for(int i=2; i<10; i++){
-            for(int j=1; j<10; j++)
-                System.out.println(i + "x" + j + "=" + i*j);
-            
+        int i = 2, j;
+
+        while(i<10)
+        {
+            j=1;
+            while(j<10){
+            System.out.println(i + "x" + j + "=" + i*j);
+            j++;
+            }
+        i++;
+        }
         }
     }
-}
---
+++ study01/First-Java-Review/ch05/ByTimes3.java
@@ -0,0 +1,6 @@
+//for문 while문을 사용해서 구구단을 출력하라. 각 한번씩
+class ByTimes3{
+    public static void main(String[] args){
+
+    }
+}
