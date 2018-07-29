ba30391dd251a474008ab7c3a016d2cdf1309a90
MIAE <kma613@naver.com>
2018-07-29 23:59:33 +0900

    '.'

--- study01/First-Java-Review/ch06/MethodDefAdd.java
+++ study01/First-Java-Review/ch06/MethodDefAdd.java
@@ -1,5 +1,11 @@
 class MethodDefAdd{
-    public void main(String[] args){
-        
+    public static void main(String[] args){
+        System.out.println("프로그램의 시작");
+        hiEveryone(12);
+        hiEveryone(13);
+        System.out.println("프로그램의 끝");
+
     }
+
+    public static
 }
